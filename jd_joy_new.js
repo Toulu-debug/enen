244ac8c7d00f8f8435cc8850e883a975515f1669
Ca11back <steliske@gmail.com>
2021-06-26 09:41:13 +0800

    Revert "Add try catch on injectToRequest()"
    This reverts commit ce551fc0511c8841dd6007f00e3d65c81b3c370d.

--- jd_joy_new.js
+++ jd_joy_new.js
@@ -526,14 +526,8 @@ function injectToRequest(fn) {
 }
 
 let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, newShareCodes, allMessage = '';
-
-try{
-  $.get = injectToRequest($.get.bind($))
-  $.post = injectToRequest($.post.bind($))
-}catch{
-  $.log('', `${$.name}:injectToRequest失败! 原因: ${e}!`, '')
-  console.log(e);
-}
+$.get = injectToRequest($.get.bind($))
+$.post = injectToRequest($.post.bind($))
 
 !(async () => {
   await requireConfig();
