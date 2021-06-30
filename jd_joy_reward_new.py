4a7240b470a8981e45bd2c63928134b4468d8529
JDHelloWorld <2860993159@qq.com>
2021-06-30 11:35:33 +0800

    独立env设置变量

--- jd_joy_reward_new.py
+++ jd_joy_reward_new.py
@@ -2,31 +2,29 @@
 宠汪汪兑换Pro
 更新时间：2021-06-30
 
-在文件中设置cookie
-cookies = ['ck1','ck2','ck3']
-或环境变量JD_COOKIE以&分割
-export JD_COOKIE="ck1&ck2&ck3"
+在env.py中设置变量
 
 cron 59 7,15,23 * * * * 或 0 0,8,16 * * *
 """
 
 import json
 import os
+import random
 import sys
 import threading
 import time
 import datetime
 import requests
+from env import *
 
 
 def main(cookie, validate):
-    target = 500 or os.environ.get('JD_JOY_REWARD_NAME')
     headers = {
         'Host': 'jdjoy.jd.com',
         'accept': '*/*',
         'content-type': 'application/json',
         'origin': 'https://h5.m.jd.com',
-        "User-Agent": '23',
+        "User-Agent": USER_AGENTS[random.randint(0, len(USER_AGENTS))],
         'referer': 'https://jdjoy.jd.com/',
         'accept-language': 'zh-cn',
         'cookie': cookie
@@ -44,7 +42,7 @@ def main(cookie, validate):
 
     for bean in config:
         sys.stdout.write(f"{bean['id']} {bean['giftName']} {bean['leftStock']}\n")
-        if bean['giftValue'] == target:
+        if bean['giftValue'] == JD_JOY_REWARD_NAME:
             while 1:
                 if datetime.datetime.now().second == 0:
                     break
@@ -53,7 +51,7 @@ def main(cookie, validate):
             url = f"https://jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE&validate={validate}"
             data = {"buyParam": {"orderSource": 'pet', "saleInfoId": bean['id']}, "deviceInfo": {}}
             res = requests.post(url, headers=headers, data=json.dumps(data)).json()
-            sys.stdout.write(json.dumps(res) + '\n')
+            sys.stdout.write(json.dumps(res, ensure_ascii=False) + '\n')
             if res['errorCode'] == 'buy_success':
                 sys.stdout.write(f"cookie{cookie.split('pt_pin=')[1].replace(';', '')}兑换成功\n")
     lock.release()
@@ -61,10 +59,6 @@ def main(cookie, validate):
 
 if __name__ == '__main__':
     print("🔔宠汪汪兑换Pro,开始！")
-    # cookies = ['ck1','ck2','ck3']
-    cookies = []
-    if os.environ.get("JD_COOKIE"):
-        cookies.append(os.environ.get("JD_COOKIE").split('&'))
     lock = threading.BoundedSemaphore(20)
     if 'test' in os.getcwd():
         path = '..'
