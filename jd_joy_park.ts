2e4bc4b9968b2b55f99eca138f13797fc8cc4bb6
JDHelloWorld <2860993159@qq.com>
2021-07-06 00:46:45 +0800

    try:任务火爆 catch:pass

--- jd_joy_park.ts
+++ jd_joy_park.ts
@@ -3,6 +3,8 @@ import USER_AGENT from './TS_USER_AGENTS';
 
 let $: any = {};
 let cookie: string = '', cookiesArr: Array<string> = [], res: any = '';
+let joyId: Array<number> = [], workJoyInfoList: any = [];
+let joyId1: number;
 
 !(async () => {
   await requireConfig();
@@ -16,26 +18,46 @@ let cookie: string = '', cookiesArr: Array<string> = [], res: any = '';
     await TotalBean();
     console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
 
+    // let joy: any = await joyList();
+    // if (joy.data.activityJoyList.length !== 0) {
+    //   joyId1 = joy.data.activityJoyList[0].id
+    //   console.log(joy.data.activityJoyList)
+    //   1:种田  2:出了
+    // res = await api('joyMove', {"joyId": joyId1, "location": 0, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
+    // console.log(res)
+    // }
+
     let taskVos: any = await api('apTaskList', {"linkId": "LsQNxL7iWDlXUs6cFl-AAg"});
     let tasks: any = taskVos.data
     for (let t of tasks) {
       if (t.taskTitle === '汪汪乐园签到') {
-        // res = await api('apDoTask', {"taskType": t.taskType, "taskId": t.id, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
-        // console.log(res)
-        // await wait(1000)
+        if (t.taskDoTimes === 0) {
+          res = await api('apDoTask', {"taskType": t.taskType, "taskId": t.id, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
+          console.log('签到:', res)
+          await wait(1000)
+          await api('apTaskDrawAward', {"taskType": t.taskType, "taskId": t.id, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
+        }
       } else if (t.taskTitle === '汪汪乐园浏览会场' || t.taskTitle === '汪汪乐园浏览商品') {
         let arr: Array<string> = ['汪汪乐园浏览会场', '汪汪乐园浏览商品']
         for (let name of arr) {
+          if (t.taskDoTimes + 1 === t.taskLimitTimes || t.taskDoTimes === t.taskLimitTimes) continue
           let times: number = name === '汪汪乐园浏览会场' ? 5 : 10;
           res = await api('apTaskDetail', {"taskType": t.taskType, "taskId": t.id, "channel": 4, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
           let apTaskDetail: any, taskResult: any, awardRes: any;
 
+          console.log(res.data)
+
           for (let i = 0; i < times; i++) {
-            apTaskDetail = res.data.taskItemList[i]
-            console.log(apTaskDetail)
+            try {
+              apTaskDetail = res.data.taskItemList[i]
+            } catch (e) {
+              break
+            }
+            console.log('apTaskDetail:', apTaskDetail)
             taskResult = await api('apDoTask', {"taskType": t.taskType, "taskId": t.id, "channel": 4, "linkId": "LsQNxL7iWDlXUs6cFl-AAg", "itemId": encodeURIComponent(apTaskDetail.itemId)})
-            console.log('doTask: ',JSON.stringify(taskResult))
-            if(taskResult.errMsg === '任务已完成') break
+            console.log('doTask: ', JSON.stringify(taskResult))
+            if (taskResult.errMsg === '任务已完成') break
+            console.log('等待中...')
             await wait(10000)
             awardRes = await api('apTaskDrawAward', {"taskType": t.taskType, "taskId": t.id, "linkId": "LsQNxL7iWDlXUs6cFl-AAg"})
             if (awardRes.success && awardRes.code === 0)
@@ -68,12 +90,23 @@ function api(fn: string, body: Object): Object {
   })
 }
 
-function wait(t:number){
-  return new Promise<void>(resolve => {
-    setTimeout(()=>{
-      resolve()
-    },t)
+function joyList() {
+  return new Promise(async resolve => {
+    let {data} = await axios.get(`https://api.m.jd.com/?functionId=joyList&body={%22linkId%22:%22LsQNxL7iWDlXUs6cFl-AAg%22}&_t=${Date.now()}&appid=activities_platform`, {
+      headers: {
+        'host': 'api.m.jd.com',
+        'User-agent': USER_AGENT,
+        'cookie': cookie,
+        'origin': 'https://joypark.jd.com',
+        'referer': 'https://joypark.jd.com'
+      }
+    })
+    resolve(data)
   })
+}
+
+function wait(t: number) {
+  return new Promise(e => setTimeout(e, t))
 }
 
 function requireConfig() {
