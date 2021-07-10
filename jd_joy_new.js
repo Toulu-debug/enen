769644db3d278170625ae0bed0b25f1a937d8a16
JDHelloWorld <2860993159@qq.com>
2021-07-10 23:08:47 +0800

    列出可偷狗粮

--- jd_joy_new.js
+++ jd_joy_new.js
@@ -561,6 +561,9 @@ $.post = injectToRequest($.post.bind($))
       message = '';
       subTitle = '';
 
+      await getFriends();
+      break
+
       await run('detail/v2');
       await run();
       await feed();
@@ -879,6 +882,44 @@ function run(fn = 'match') {
       } finally {
         resolve();
       }
+    })
+  })
+}
+
+function getFriends() {
+  return new Promise((resolve) => {
+    $.post({
+      url: 'https://jdjoy.jd.com/common/pet/enterRoom/h5?invitePin=&reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE',
+      headers: {
+        'Host': 'jdjoy.jd.com',
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'com.jingdong.app.mall',
+        'Referer': 'https://h5.m.jd.com/babelDiy/Zeus/2wuqXrZrhygTQzYA7VufBEpj4amH/index.html?babelChannel=ttt12&sid=445902658831621c5acf782ec27ce21w&un_area=12_904_3373_62101',
+        'Origin': 'https://h5.m.jd.com',
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+        'Cookie': cookie
+      },
+      body: JSON.stringify({})
+    }, async (err, resp, data) => {
+      await $.wait(1000)
+      $.get({
+        url: 'https://jdjoy.jd.com/common/pet/h5/getFriends?itemsPerPage=20&currentPage=2&reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE',
+        headers: {
+          'Host': 'jdjoy.jd.com',
+          'Accept': '*/*',
+          'Referer': 'https://h5.m.jd.com/babelDiy/Zeus/2wuqXrZrhygTQzYA7VufBEpj4amH/index.html',
+          "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+          'cookie': cookie
+        }
+      }, (err, resp, data) => {
+        data = JSON.parse(data)
+        for (let f of data.datas) {
+          if (f.stealStatus === 'can_steal') {
+            console.log('可偷:', f.friendPin)
+          }
+        }
+        resolve();
+      })
     })
   })
 }
