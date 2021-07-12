1b1aba64d98fef75538a707c329cab0757c42952
JDHelloWorld <2860993159@qq.com>
2021-07-12 17:01:30 +0800

    who are you

--- jd_update.js
+++ jd_update.js
@@ -12,7 +12,7 @@ exec("cd /ql/repo/JDHelloWorld_jd_scripts; git fetch --all; git reset --hard ori
 })
 
 if (__dirname.indexOf('/ql/') > -1) {
-  exec('ql repo https://github.com/JDHelloWorld/jd_scripts "jd_|jx_|getJDCookie" "activity|backUp" "^jd[^_]|USER"', (error, stdout, stderr) => {
+  exec('ql repo https://github.com/JDHelloWorld/jd_scripts.git "jd_|jx_|getJDCookie" "activity|backUp|Coupon|enen" "^jd[^_]|USER"', (error, stdout, stderr) => {
     console.log(1, error)
     console.log(2, stdout.trim())
     console.log(3, stderr)
