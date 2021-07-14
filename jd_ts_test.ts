d3c44aa122a7eb3ee1e775d45d48dc215beea117
JDHelloWorld <2860993159@qq.com>
2021-07-14 22:49:31 +0800

    rm

--- jd_ts_test.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config()
-
-let CFD_HELP_HW: string | boolean = process.env.CFD_HELP_HW ? process.env.CFD_HELP_HW : false;
-console.log('财富岛助力HelloWorld:', CFD_HELP_HW)
-
-let CFD_HELP_POOL: boolean | string = process.env.CFD_HELP_POOL ? process.env.CFD_HELP_POOL : true;
-console.log('财富岛帮助助力池:', CFD_HELP_POOL)
