ea3873e6f813f27e9f1118d0f3db59a092d2e7e0
JDHelloWorld <2860993159@qq.com>
2021-07-26 01:37:03 +0800

    ???

--- sendNotify.js
+++ sendNotify.js
@@ -43,9 +43,9 @@ let TG_API_HOST = 'api.telegram.org'
 // =======================================钉钉机器人通知设置区域===========================================
 //此处填你钉钉 bot 的webhook，例如：5a544165465465645d0f31dca676e7bd07415asdasd
 //(环境变量名 DD_BOT_TOKEN)
-let DD_BOT_TOKEN = '9d0ef4435436d2141becb4af3d734c188172085c34dff094f8122585c33b334b';
+let DD_BOT_TOKEN = '';
 //密钥，机器人安全设置页面，加签一栏下面显示的SEC开头的字符串
-let DD_BOT_SECRET = 'SEC22ec5bbf3f70aff91b32ac9a70b94686ce5a010cd6659d0d10403ce69ffb529f';
+let DD_BOT_SECRET = '';
 
 // =======================================企业微信机器人通知设置区域===========================================
 //此处填你企业微信机器人的 webhook(详见文档 https://work.weixin.qq.com/api/doc/90000/90136/91770)，例如：693a91f6-7xxx-4bc4-97a0-0ec2sifa5aaa
