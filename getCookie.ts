b579ea82e4020b47dc836657eea9c8451ff8b487
JDHelloWorld <2860993159@qq.com>
2021-07-28 19:07:22 +0800

    Random UA

--- getCookie.ts
+++ getCookie.ts
@@ -3,9 +3,9 @@
  * export PUSH_COOKIE=true
  */
 import axios from 'axios'
-// import USER_AGENT from './TS_USER_AGENTS'
+import USER_AGENT from './TS_USER_AGENTS'
 
-const USER_AGENT = 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
+// const USER_AGENT = 'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36'
 const qrcode = require('qrcode-terminal')
 const notify = require('./sendNotify')
 let PUSH_COOKIE = process.env.PUSH_COOKIE ?? false
