5e618b5d49a477c07a599e1a6204e6c61ab99595
JDHelloWorld <2860993159@qq.com>
2021-08-09 22:11:20 +0800

    查看助力码和助力次数

--- tools/jd_getShareCodes.ts
+++ tools/jd_getShareCodes.ts
@@ -1,6 +1,12 @@
-import axios from 'axios';
-import USER_AGENT, {requireConfig, TotalBean} from './TS_USER_AGENTS';
+/**
+ * cron: 0 23 * * *
+ * 查看助力码和助力次数
+ */
 
+import axios from 'axios';
+import USER_AGENT, {requireConfig, TotalBean} from '../TS_USER_AGENTS';
+
+const notify = require('../sendNotify')
 let cookie: string = '', res: any = '', UserName: string, index: number;
 
 !(async () => {
@@ -11,15 +17,18 @@ let cookie: string = '', res: any = '', UserName: string, index: number;
     UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
     index = i + 1;
     let {isLogin, nickName}: any = await TotalBean(cookie)
+    if (!isLogin) {
+      notify.sendNotify(__filename.split('/').pop(), `cookie已失效\n京东账号${index}：${nickName || UserName}`)
+      continue
+    }
     console.log(`\n开始【京东账号${index}】${nickName || UserName}\n`);
 
-    res = await api()
-    console.log('carnivalcity:', res.data.shareId)
+    await carnivalcity()
   }
 })()
 
-async function api() {
-  let {data} = await axios.post('https://api.m.jd.com/api',
+async function carnivalcity() {
+  res = await axios.post('https://api.m.jd.com/api',
     `appid=guardian-starjd&functionId=carnivalcity_jd_prod&body=${escape(JSON.stringify({apiMapping: "/khc/task/getSupport"}))}&t=${Date.now()}&loginType=2`, {
       headers: {
         "Accept": "application/json, text/plain, */*",
@@ -33,5 +42,21 @@ async function api() {
         "User-Agent": USER_AGENT,
       }
     })
-  return data
+  console.log('carnivalcity:', res.data.data.shareId)
+
+  res = await axios.post('https://api.m.jd.com/api',
+    `appid=guardian-starjd&functionId=carnivalcity_jd_prod&body=${escape(JSON.stringify({apiMapping: "/khc/index/supportList"}))}&t=${Date.now()}&loginType=2`, {
+      headers: {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-cn",
+        "Connection": "keep-alive",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Origin": "https://carnivalcity.m.jd.com",
+        "Referer": "https://carnivalcity.m.jd.com/",
+        "Cookie": cookie,
+        "User-Agent": USER_AGENT,
+      }
+    })
+  console.log('被助力：', res.data.data.supportedNums, '/', res.data.data.supportNeedNums)
 }
