572c436c623bc66c892806ab8f4d90785e6c0615
JDHelloWorld <2860993159@qq.com>
2021-08-21 17:52:04 +0800

    getInvokeKey

---
+++ tools/jd_joy_getInvokeKey.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+!(async () => {
+  let res: any = await api('https://prodev.m.jd.com/mall/active/2tZssTgnQsiUqhmg5ooLSHY9XSeN/index.html')
+  let file: string = 'https://storage.360buyimg.com/' + res.match(/htmlSourceUrl":"([^"]*)/)[1]
+  res = await api(file)
+  file = 'https:' + res.match(/src="([^"]*)/)[1]
+  res = await api(file)
+  file = res.match(/h=n\(90\),v="([^"]*)/)[1]
+  console.log('invokeKey:', file)
+})()
+
+async function api(url: string) {
+  let {data} = await axios.get(url, {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+    }
+  })
+  return data
+}
