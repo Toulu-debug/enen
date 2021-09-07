5677e2a5bba47fbd111ee9a4560feba6650483bd
JDHelloWorld <2860993159@qq.com>
2021-09-08 02:18:30 +0800

    牧场兑换新品通知

--- jd_jxmc_stock.ts
+++ jd_jxmc_stock.ts
@@ -5,17 +5,16 @@
  */
 
 import axios from 'axios';
-import {requireConfig, requestAlgo, decrypt} from './TS_USER_AGENTS';
+import {requireConfig, requestAlgo, decrypt, getJxToken, wait, getRandomNumberByRange} from './TS_USER_AGENTS';
 import {readFileSync, writeFileSync, accessSync} from "fs";
 
 const notify = require('./sendNotify')
-
 let cookie: string = '', res: any = '', UserName: string;
 
 !(async () => {
   await requestAlgo();
   let cookiesArr: any = await requireConfig();
-  cookie = cookiesArr[0];
+  cookie = cookiesArr[getRandomNumberByRange(0, cookiesArr.length)];
   UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
   try {
     accessSync('./jxmc_stock.json')
@@ -23,8 +22,18 @@ let cookie: string = '', res: any = '', UserName: string;
     writeFileSync('./jxmc_stock.json', '{}', 'utf-8')
   }
   let exist: any = JSON.parse(readFileSync('./jxmc_stock.json', 'utf-8'))
-  let items: string = '', message: string = '';
-  res = await api('queryservice/GetGoodsListV2', 'channel,sceneid')
+  let items: string = '', message: string = '', token = getJxToken(cookie);
+  res = await api('queryservice/GetGoodsListV2',
+    'activeid,activekey,channel,jxmc_jstoken,phoneid,sceneid,timestamp', {
+      activeid: 'jxmc_active_0001',
+      activekey: 'null',
+      jxmc_jstoken: token.strPgUUNum,
+      timestamp: token.strPgtimestamp,
+      phoneid: token.strPhoneID
+    })
+
+  await wait(2000);
+
   for (let good of res.data.goodslist) {
     if (!Object.keys(exist).includes(good.prizepool)) {
       items += good.prizepool + ','
@@ -34,15 +43,31 @@ let cookie: string = '', res: any = '', UserName: string;
       }
     }
   }
+  let allItems: string = items;
   if (items) {
-    res = await getEgg(items)
-    for (let t of res.result) {
-      exist[t.active].name = t.prizes[0].Name
+    let arr: string[] = items.split(',');
+    arr.pop();
+    items = '';
+    let result = [];
+    for (let i = 0, len = arr.length; i < len; i += 30) {
+      result.push(arr.slice(i, i + 30))
+    }
+    for (let group of result) {
+      for (let id of group) {
+        items += id + ','
+      }
+      res = await getEgg(items)
+      await wait(1000)
+      for (let t of res.result) {
+        console.log(t.prizes[0].Name)
+        exist[t.active].name = t.prizes[0].Name
+      }
+      items = ''
     }
   }
   writeFileSync('./jxmc_stock.json', JSON.stringify(exist, null, 2), 'utf-8')
   for (let j of Object.keys(exist)) {
-    if (items.indexOf(j) > -1) {
+    if (allItems.indexOf(j) > -1) {
       message += exist[j].name + '\t' + exist[j].egg + '\n'
     }
   }
@@ -54,7 +79,11 @@ let cookie: string = '', res: any = '', UserName: string;
 
 interface Params {
   isgift?: number,
-
+  activeid?: string,
+  activekey?: string,
+  jxmc_jstoken?: string,
+  timestamp?: string,
+  phoneid?: string
 }
 
 function api(fn: string, stk: string, params: Params = {}) {
@@ -85,16 +114,18 @@ function api(fn: string, stk: string, params: Params = {}) {
 }
 
 function getEgg(items: string) {
+  items = items.substr(0, items.length - 1)
+  let rnd = "abcdefhijkmnprstwxyz".charAt(Math.floor(Math.random() * 4)).toUpperCase();
   return new Promise(async resolve => {
-    let {data} = await axios.get(`https://m.jingxi.com/active/queryprizedetails?actives=${items}&_=${Date.now()}&sceneval=2`, {
+    let {data} = await axios.get(`https://m.jingxi.com/active/queryprizedetails?actives=${items}&_=${Date.now()}&sceneval=2&g_login_type=1&callback=jsonpCBK${rnd}&g_ty=ls`, {
       headers: {
         'Cookie': cookie,
         'Host': 'm.jingxi.com',
         'User-Agent': 'jdpingou;',
-        'Referer': 'https://st.jingxi.com/',
+        'Referer': 'https://st.jingxi.com/pingou/jxmc/index.html',
       }
     })
-    data = JSON.parse(data.replace('try{ QueryPrizesDetails(', '').replace(');}catch(e){}', ''))
+    data = JSON.parse(data.replace(`try{ jsonpCBK${rnd}(`, '').replace(');}catch(e){}', ''))
     resolve(data)
   })
 }
