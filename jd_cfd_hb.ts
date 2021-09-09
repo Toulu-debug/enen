0f33c003ba0f5df9507ae3e0183cad84d894aaf5
JDHelloWorld <2860993159@qq.com>
2021-09-09 19:58:41 +0800

    while

--- jd_cfd_hb.ts
+++ jd_cfd_hb.ts
@@ -4,7 +4,7 @@
  * 我就是看看，不抢
  */
 import axios from 'axios';
-import USER_AGENT, {requireConfig, requestAlgo, decrypt} from './TS_USER_AGENTS';
+import USER_AGENT, {requireConfig, requestAlgo, decrypt, wait} from './TS_USER_AGENTS';
 import * as dotenv from 'dotenv';
 
 dotenv.config()
@@ -15,6 +15,12 @@ let cookie: string = '', cookiesArr: any, res: any = '';
   cookiesArr = await requireConfig();
   cookie = cookiesArr[0]
   res = await api('user/ExchangeState', '_cfd_t,bizCode,dwEnv,dwType,ptag,source,strZone', {dwType: '2'})
+  while(1) {
+    if (new Date().getSeconds() < 15)
+      break
+    else
+      await wait(50)
+  }
   for (let t of res.hongbao) {
     console.log(t.strPrizeName, 'state:', t.dwState, 'num:', t.dwStockNum)
   }
@@ -46,9 +52,3 @@ function api(fn: string, stk: string, params: Params = {}) {
     resolve(data)
   })
 }
-
-
-
-
-
-
