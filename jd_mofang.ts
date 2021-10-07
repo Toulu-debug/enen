3c8c701514516d6ab2b5f70afd8e3db5b3481ec3
JDHelloWorld <2860993159@qq.com>
2021-10-07 17:01:38 +0800

    mofang

--- jd_mofang.ts
+++ jd_mofang.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 import USER_AGENT, {requireConfig, wait} from './TS_USER_AGENTS';
 
-let cookie: string = '', res: any = '', UserName: string, index: number, shareCodes: string[] = [], shareCodesHbInterval: string[] = [];
+let cookie: string = '', res: any = '', UserName: string, index: number;
+let shareCodeSelf: {
+  encryptProjectId: string,
+  encryptAssignmentId: string,
+  itemId: string
+} [] = [];
+
+const DEBUG = false;
 
 !(async () => {
   let cookiesArr: any = await requireConfig();
@@ -13,11 +20,30 @@ let cookie: string = '', res: any = '', UserName: string, index: number, shareCo
 
     res = await api("functionId=getInteractionHomeInfo&body=%7B%22sign%22%3A%22u6vtLQ7ztxgykLEr%22%7D&appid=content_ecology&client=wh5&clientVersion=1.0.0")
     let sign: string = res.result.taskConfig.projectId
+    DEBUG ? console.log(JSON.stringify(res)) : ''
+
     console.log('sing:', sign)
     res = await api(`functionId=queryInteractiveInfo&body=%7B%22encryptProjectId%22%3A%22${sign}%22%2C%22sourceCode%22%3A%22acexinpin0823%22%2C%22ext%22%3A%7B%7D%7D&client=wh5&clientVersion=1.0.0&appid=content_ecology`)
+    DEBUG ? console.log(JSON.stringify(res)) : ''
+
     for (let t of res.assignmentList) {
       if (t.completionCnt < t.assignmentTimesLimit) {
         if (t.ext) {
+          if (t.ext.extraType === 'assistTaskDetail') {
+            console.log('助力码:', t.ext.assistTaskDetail.itemId)
+            shareCodeSelf.push({
+              encryptProjectId: sign,
+              encryptAssignmentId: t.encryptAssignmentId,
+              itemId: t.ext.assistTaskDetail.itemId
+            })
+          }
+          if (t.assignmentName === '每日签到') {
+            let signDay: number = t.ext.sign1.signList?.length || 0,
+              type: number = t.rewards[signDay].rewardType
+            console.log(signDay, type)
+            // TODO 签到
+          }
+
           for (let proInfo of t.ext.productsInfo ?? []) {
             if (proInfo.status === 1) {
               console.log(t.assignmentName)
@@ -49,19 +75,35 @@ let cookie: string = '', res: any = '', UserName: string, index: number, shareCo
             if (proInfo.status === 1) {
               console.log(t.assignmentName)
               res = await api(`functionId=doInteractiveAssignment&body=%7B%22encryptProjectId%22%3A%22${sign}%22%2C%22encryptAssignmentId%22%3A%22${t.encryptAssignmentId}%22%2C%22sourceCode%22%3A%22acexinpin0823%22%2C%22itemId%22%3A%22${proInfo.itemId}%22%2C%22actionType%22%3A1%2C%22completionFlag%22%3A%22%22%2C%22ext%22%3A%7B%7D%7D&client=wh5&clientVersion=1.0.0&appid=content_ecology`)
-              console.log(res)
+              console.log('加购:', res)
               if (res.msg === '任务已完成')
                 break
-              res = await api(`functionId=doInteractiveAssignment&body=%7B%22encryptProjectId%22%3A%22${sign}%22%2C%22encryptAssignmentId%22%3A%22${t.encryptAssignmentId}%22%2C%22sourceCode%22%3A%22acexinpin0823%22%2C%22itemId%22%3A%22${proInfo.itemId}%22%2C%22actionType%22%3A0%2C%22completionFlag%22%3A%22%22%2C%22ext%22%3A%7B%7D%7D&client=wh5&clientVersion=1.0.0&appid=content_ecology`)
-              console.log(res)
-              if (res.msg === '任务已完成')
-                break
+              await wait(2000)
             }
           }
         }
       }
     }
   }
+
+  console.log('助力排队:', shareCodeSelf)
+
+  cookie = cookiesArr[0];
+  UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
+  for (let code of shareCodeSelf) {
+    console.log(`账号1 ${UserName} 去助力 ${code.itemId}`)
+    res = await api(`functionId=doInteractiveAssignment&body=%7B%22encryptProjectId%22%3A%22${code.encryptProjectId}%22%2C%22encryptAssignmentId%22%3A%22${code.encryptAssignmentId}%22%2C%22sourceCode%22%3A%22acexinpin0823%22%2C%22itemId%22%3A%22${code.itemId}%22%2C%22actionType%22%3A%22%22%2C%22completionFlag%22%3A%22%22%2C%22ext%22%3A%7B%7D%7D&client=wh5&clientVersion=1.0.0&appid=content_ecology`)
+    console.log('助力结果:', res)
+    await wait(2000)
+  }
+
+  cookie = cookiesArr[1];
+  UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
+  let code: any = shareCodeSelf[0]
+  console.log(`账号2 ${UserName} 去助力 ${code.itemId}`)
+  res = await api(`functionId=doInteractiveAssignment&body=%7B%22encryptProjectId%22%3A%22${code.encryptProjectId}%22%2C%22encryptAssignmentId%22%3A%22${code.encryptAssignmentId}%22%2C%22sourceCode%22%3A%22acexinpin0823%22%2C%22itemId%22%3A%22${code.itemId}%22%2C%22actionType%22%3A%22%22%2C%22completionFlag%22%3A%22%22%2C%22ext%22%3A%7B%7D%7D&client=wh5&clientVersion=1.0.0&appid=content_ecology`)
+  console.log('助力结果:', res)
+
 })()
 
 async function api(params: any) {
