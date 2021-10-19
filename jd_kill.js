0a6deff60922bcaed75052d038feafda2ac68e1f
JDHelloWorld <2860993159@qq.com>
2021-10-19 18:48:01 +0800

    kill jxmc

---
+++ jd_kill.js
@@ -0,0 +1,9 @@
+/**
+ * cron: * * * * *
+ */
+
+const exec = require('child_process').exec
+
+exec('ps -e|grep jxmc|grep -v grep', (error, stdout, stderr) => {
+  exec(`kill -9 ${stdout.split(' ')[0]}`)
+})
