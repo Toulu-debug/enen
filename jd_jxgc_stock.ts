3ec58ce0cebb034ac792db9a788b11108a7ada52
JDHelloWorld <2860993159@qq.com>
2021-11-15 16:34:48 +0800

    jd_jxgc_stock.ts

--- jd_jxgc_stock.ts
+++ jd_jxgc_stock.ts
@@ -1,31 +1,26 @@
 /**
- * 显示京喜工厂当前可生产商品
+ * 京喜工厂：肯德基、沃尔玛
  * cron: 0 * * * *
  */
 
 import axios from "axios";
 import USER_AGENT, {h5st, requireConfig, requestAlgo} from "./TS_USER_AGENTS";
 import {sendNotify} from './sendNotify'
-import {accessSync, readFileSync, writeFileSync} from "fs";
+import {existsSync, readFileSync, writeFileSync} from "fs";
 
-let cookie: string = '', res: any = '', UserName: string, message: string = '';
+let cookie: string = '', res: any = '', message: string = '';
 
 
 !(async () => {
   await requestAlgo(10001)
   let cookiesArr: any = await requireConfig();
   cookie = cookiesArr[0];
-  UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
-  console.log(`\n开始【京东账号1】${UserName}\n`);
 
-  let exist: string[];
-  try {
-    accessSync('./json/jxgc_stock.json')
-    exist = JSON.parse(readFileSync('./json/jxgc_stock.json').toString())
-  } catch (e) {
-    exist = []
+  /*
+  let exist: string[] = [];
+  if (existsSync('json/jxgc_stock.json')) {
+    exist = JSON.parse(readFileSync('./json/jxgc_stock.json').toString() || '[]')
   }
-
   res = await api();
   let current: string[] = []
   for (let t of res.data.commodityList) {
@@ -40,11 +35,22 @@ let cookie: string = '', res: any = '', UserName: string, message: string = '';
     console.log('send...')
     sendNotify('京喜工厂可生产', message)
   }
+   */
+  res = await api();
+  let keywords: string[] = ['KFC', 'kfc', '肯德基', '沃尔玛']
+  for (let t of res.data.commodityList) {
+    let name: string = t.name
+    for (let keyword of keywords) {
+      if (name.indexOf(keyword) > -1) {
+        await sendNotify("京喜工厂", name)
+        break
+      }
+    }
+  }
 })()
 
 async function api() {
-  let url: string = `https://wq.jd.com/dreamfactory/diminfo/GetCommodityList?zone=dream_factory&flag=2&pageNo=1&pageSize=12&_time=${Date.now()}&_stk=_time%2Cflag%2CpageNo%2CpageSize%2Czone&_ste=1&_=${Date.now()}&sceneval=2`
-  url = h5st(url, '_time,flag,pageNo,pageSize,zone', {}, 10001)
+  let url: string = h5st(`https://wq.jd.com/dreamfactory/diminfo/GetCommodityList?zone=dream_factory&flag=2&pageNo=1&pageSize=12&_time=${Date.now()}&_stk=_time%2Cflag%2CpageNo%2CpageSize%2Czone&_ste=1&_=${Date.now()}&sceneval=2`, '_time,flag,pageNo,pageSize,zone', {}, 10001)
   let {data}: any = await axios.get(url, {
     headers: {
       'Host': 'wq.jd.com',
