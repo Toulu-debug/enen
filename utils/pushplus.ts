999596857ba4a74bcaadc538e2edb7f06d1cf966
JDHelloWorld <2860993159@qq.com>
2022-02-19 20:49:42 +0800

    pushplus.ts

---
+++ utils/pushplus.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import {readFileSync} from "fs";
+
+let account: { pushplus?: string, pt_pin: string }[] = JSON.parse(readFileSync("./utils/account.json").toString());
+
+export async function pushplus(content: string) {
+  for (let user of account) {
+    if (content.includes(decodeURIComponent(user.pt_pin)) && user.pushplus) {
+      await send(user.pushplus, content)
+    }
+  }
+}
+
+async function send(token: string, content: string) {
+  let {data}: any = await axios.post('https://www.pushplus.plus/send', {
+    token: token,
+    title: '京东红包',
+    content: content,
+  }, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  if (data.code === 200) {
+    console.log('pushplus发送成功')
+  }
+}
