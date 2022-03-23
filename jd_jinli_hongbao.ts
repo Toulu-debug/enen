3cf278f22f76155fed31039b9f6ed6d3fb77d04d
JDHelloWorld <jerfy286@qq.com>
2022-03-23 21:53:52 +0800

    打开红包频繁时退出

--- jd_jinli_hongbao.ts
+++ jd_jinli_hongbao.ts
@@ -9,7 +9,7 @@
 import axios from 'axios';
 import {logs} from './utils/jinli_log';
 import {sendNotify} from './sendNotify';
-import {getRandomNumberByRange, getshareCodeHW, o2s, randomString, requireConfig, wait} from "./TS_USER_AGENTS";
+import {getRandomNumberByRange, getshareCodeHW, o2s, obj2str, randomString, requireConfig, wait} from "./TS_USER_AGENTS";
 
 let cookie: string = '', cookiesArr: string[] = [], res: any = '', UserName: string, UA: string = ''
 let shareCodesSelf: string[] = [], shareCodes: string[] = [], shareCodesHW: string[] = [], fullCode: string[] = []
@@ -61,7 +61,10 @@ async function join() {
 }
 
 async function open() {
+  let exitOpen: boolean = false
   for (let [index, value] of cookiesArr.entries()) {
+    if (exitOpen)
+      break
     try {
       cookie = value
       UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
@@ -69,58 +72,10 @@ async function open() {
       UA = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random() * 4 + 10)}.${Math.ceil(Math.random() * 4)};${randomString(40)}`
       log = logs[getRandomNumberByRange(0, logs.length - 1)]
       let random = log.match(/"random":"(\d+)"/)[1], log1 = log.match(/"log":"(.*)"/)[1]
-      // 做任务
-      /*
-      res = await api('taskHomePage', {})
-      await wait(1000)
-      for (let t of res.data.result.taskInfos) {
-        if (!t.alreadyReceivedCount || t.alreadyReceivedCount < t.requireCount) {
-          if ([2, 3, 4, 5, 8].includes(t.taskType)) {
-            res = await api('startTask', {"taskType": t.taskType, "random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~1orj8k3`, "sceneid": "JLHBhPageh5"})
-            console.log(t.title, res.data.biz_msg)
-            await wait(1000)
-            res = await api('getTaskDetailForColor', {taskType: t.taskType})
-            await wait(1000)
-            for (let tp of res.data.result.advertDetails) {
-              if (tp.status === 0) {
-                res = await api('taskReportForColor', {"taskType": t.taskType, "detailId": tp.id})
-                console.log(t.title, tp.name, res.data.biz_msg)
-                await wait(1000)
-              }
-            }
-          }
-        }
-        if (t.innerStatus === 3) {
-          res = await api('h5receiveRedpacketAll', {"taskType": t.taskType, "random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~138q6w6`, "sceneid": "JLHBhPageh5"})
-          console.log(`${t.title} 打开成功，获得`, parseFloat(res.data.result.discount))
-          if (!min.includes(parseFloat(res.data.result.discount)))
-            await sendNotify(`锦鲤红包`, `账号${index + 1} ${UserName}\n${res.data.result.discount}`)
-          await wait(1000)
-        }
-      }
-      await wait(1000)
-
-
-      // 打开任务红包
-      res = await api('taskHomePage', {})
-      await wait(1000)
-      for (let t of res.data.result.taskInfos) {
-        if (t.innerStatus === 3) {
-          res = await api('h5receiveRedpacketAll', {"taskType": t.taskType, "random": getRandomNumberByRange(36135846, 74613584), "log": `${Date.now()}~138q6w6`, "sceneid": "JLHBhPageh5"})
-          console.log(`${t.title} 打开成功，获得`, parseFloat(res.data.result.discount))
-          if (!min.includes(parseFloat(res.data.result.discount)))
-            await sendNotify(`锦鲤红包`, `账号${index + 1} ${UserName}\n${res.data.result.discount}`)
-          await wait(1000)
-        }
-      }
-      await wait(2000)
-       */
 
       // 打开助力红包
       let j: number = 1
       res = await api('h5activityIndex', {"isjdapp": 1})
-      o2s(res)
-      await wait(5000)
       for (let t of res.data.result.redpacketConfigFillRewardInfo) {
         if (t.packetStatus === 2) {
           console.log(`红包${j}已拆过，获得`, t.packetAmount)
@@ -130,6 +85,9 @@ async function open() {
         } else if (t.packetStatus === 1) {
           console.log(`红包${j}可拆`)
           res = await api('h5receiveRedpacketAll', {"random": random, "log": log1, "sceneid": "JLHBhPageh5"})
+          if (obj2str(res) === '{}') {
+            exitOpen = true
+          }
           console.log(res.data.biz_msg, parseFloat(res.data.result.discount))
           await wait(10000)
         } else {
