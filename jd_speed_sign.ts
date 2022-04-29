8e066485b192e26732b3a0ab9d9e8a54edf8968e
JDHelloWorld <jerfy286@qq.com>
2022-04-29 16:02:50 +0800

    jd_speed_sign.ts

--- jd_speed_sign.ts
+++ jd_speed_sign.ts
@@ -1,7 +1,10 @@
-import axios from 'axios'
-import {requireConfig, wait, o2s} from './TS_USER_AGENTS'
-import {requestAlgo, geth5st} from "./utils/V3";
-import {SHA256} from "crypto-js";
+/**
+ * 极速版-签到+提现
+ * cron: 45 0 * * *
+ */
+
+import {requireConfig, wait, post} from './TS_USER_AGENTS'
+import {H5ST} from "./h5st";
 
 let cookie: string = '', res: any = '', UserName: string = ''
 
@@ -9,7 +12,6 @@ let cookie: string = '', res: any = '', UserName: string = ''
   let cookiesArr: any = await requireConfig()
   for (let [index, value] of cookiesArr.entries()) {
     try {
-      await requestAlgo('15097', 'jdltapp;')
       cookie = value
       UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
       console.log(`\n开始【京东账号${index + 1}】${UserName}\n`)
@@ -31,7 +33,7 @@ let cookie: string = '', res: any = '', UserName: string = ''
         }
       }
     } catch (e) {
-      console.log(e)
+      console.log('error', typeof e)
     }
     await wait(2000)
   }
@@ -39,23 +41,20 @@ let cookie: string = '', res: any = '', UserName: string = ''
 
 async function api(fn: string, body: object) {
   let timestamp: number = Date.now(), t: { key: string, value: string } [] = [
-    {key: 'functionId', value: fn},
-    {key: 'body', value: SHA256(JSON.stringify(body)).toString()},
-    {key: 't', value: timestamp.toString()},
     {key: 'appid', value: 'activities_platform'},
+    {key: 'body', value: JSON.stringify(body)},
     {key: 'client', value: 'H5'},
     {key: 'clientVersion', value: '1.0.0'},
+    {key: 'functionId', value: fn},
+    {key: 't', value: timestamp.toString()},
   ]
-  let h5st = geth5st(t, '8adfb')
-  let {data} = await axios.post('https://api.m.jd.com/', `functionId=${fn}&body=${JSON.stringify(body)}&t=${timestamp}&appid=activities_platform&client=H5&clientVersion=1.0.0&h5st=${h5st}`, {
-    headers: {
-      'Host': 'api.m.jd.com',
-      'Origin': 'https://daily-redpacket.jd.com',
-      'User-Agent': 'jdltapp;',
-      'Referer': 'https://daily-redpacket.jd.com/',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Cookie': cookie
-    }
+  let h5st: string = await new H5ST(t, "15097", "jdltapp;", "8317250570595470").__run()
+  return await post('https://api.m.jd.com/', `functionId=${fn}&body=${encodeURIComponent(JSON.stringify(body))}&t=${timestamp}&appid=activities_platform&client=H5&clientVersion=1.0.0&h5st=${h5st}`, {
+    'Host': 'api.m.jd.com',
+    'User-Agent': 'jdltapp;android;3.8.16;',
+    'Origin': 'https://daily-redpacket.jd.com',
+    'Referer': 'https://daily-redpacket.jd.com/',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cookie': cookie
   })
-  return data
 }
