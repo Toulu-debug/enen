274db9fea3e14dd7a1ad2b1b6ffabe6f0c29a2c2
JDHelloWorld <jerfy286@qq.com>
2022-05-06 17:39:58 +0800

    runningPrizeDraw

--- jd_joy_park_run.ts
+++ jd_joy_park_run.ts
@@ -11,7 +11,7 @@ import {existsSync, readFileSync} from "fs";
 import {getDate} from "date-fns";
 
 let cookie: string = '', res: any = '', UserName: string = ''
-let assets: number = 0.04, captainId: string = '', h5stTool: H5ST = new H5ST('b6ac3', 'jdltapp;', '1804945295425750')
+let assets: number = 0, captainId: string = '', h5stTool: H5ST = new H5ST('b6ac3', 'jdltapp;', '1804945295425750')
 
 !(async () => {
   let cookiesArr: string[] = await requireConfig()
@@ -40,7 +40,7 @@ let assets: number = 0.04, captainId: string = '', h5stTool: H5ST = new H5ST('b6
 
     try {
       res = await team('runningMyPrize', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "pageSize": 20, "time": null, "ids": null})
-      let sum: number = 0
+      let sum: number = 0, rewardAmount: number = res.data.rewardAmount
       for (let t of res.data.detailVos) {
         if (getDate(new Date(t.createTime)) === new Date().getDate()) {
           sum = add(sum, t.amount)
@@ -49,6 +49,18 @@ let assets: number = 0.04, captainId: string = '', h5stTool: H5ST = new H5ST('b6
         }
       }
       console.log('今日收益', sum)
+      if (res.data.runningCashStatus.currentEndTime) {
+        if (res.data.runningCashStatus.status === 0) {
+          console.log('可提现', rewardAmount)
+          res = await api('runningPrizeDraw', {"linkId": "L-sOanK_5RJCz7I314FpnQ", "type": 2})
+          await wait(2000)
+          console.log(res.data.message)
+        } else {
+          console.log('已提现')
+        }
+      } else {
+        console.log('非提现时段')
+      }
 
       await h5stTool.__genAlgo()
       res = await team('runningTeamInfo', {"linkId": "L-sOanK_5RJCz7I314FpnQ"})
