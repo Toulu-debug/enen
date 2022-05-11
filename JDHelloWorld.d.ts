fbbc2bd736c97789bf06f25f84f80d417e672512
JDHelloWorld <jerfy286@qq.com>
2022-05-11 21:27:32 +0800

    JDHelloWorld.d.ts

--- JDHelloWorld.d.ts
+++ JDHelloWorld.d.ts
@@ -1,11 +1,19 @@
+interface User {
+    i: number;
+    UserName: string;
+    cookie: string;
+    UserAgent: string;
+}
 declare class JDHelloWorld {
     getCookie(check?: boolean): Promise<string[]>;
     checkCookie(cookie: string): Promise<boolean>;
+    exceptCookie(filename?: string): any;
     get(url: string, headers?: any): Promise<unknown>;
-    post(url: string, data: any, headers?: any): Promise<unknown>;
-    wait(timeout?: number): Promise<unknown>;
+    post(url: string, data: any, headers?: any): Promise<object>;
+    wait(ms?: number): Promise<unknown>;
+    o2s(obj: object): void;
     run(son: {
-        main: any;
+        main: Function;
     }): Promise<void>;
 }
-export { JDHelloWorld };
+export { User, JDHelloWorld };
