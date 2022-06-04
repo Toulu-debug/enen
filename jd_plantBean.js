ddaa23ff81be57e35270d1bc40fb10d04de1ec33
JDHelloWorld <jerfy286@qq.com>
2022-06-04 22:34:51 +0800

    jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -113,7 +113,6 @@ async function jdPlantBean() {
       await receiveNutrients();//定时领取营养液
       await doHelp();//助力
       await doTask();//做日常任务
-      await doEgg();
       await stealFriendWater();
       await doCultureBean();
       await doGetReward();
@@ -230,25 +229,6 @@ async function stealFriendWater() {
     }
   } else {
     console.log(`$.stealFriendList 异常： ${JSON.stringify($.stealFriendList)}`)
-  }
-}
-
-async function doEgg() {
-  await egg();
-  if ($.plantEggLotteryRes && $.plantEggLotteryRes.code === '0') {
-    if ($.plantEggLotteryRes.data.restLotteryNum > 0) {
-      const eggL = new Array($.plantEggLotteryRes.data.restLotteryNum).fill('');
-      console.log(`目前共有${eggL.length}次扭蛋的机会`)
-      for (let i = 0; i < eggL.length; i++) {
-        console.log(`开始第${i + 1}次扭蛋`);
-        await plantEggDoLottery();
-        console.log(`天天扭蛋成功：${JSON.stringify($.plantEggDoLotteryResult)}`);
-      }
-    } else {
-      console.log('暂无扭蛋机会')
-    }
-  } else {
-    console.log('查询天天扭蛋的机会失败' + JSON.stringify($.plantEggLotteryRes))
   }
 }
 
