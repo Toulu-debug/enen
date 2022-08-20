56e25d4195c59bfbfa1b2c8ba1bbb596b5acc9a9
JDHelloWorld <jerfy286@qq.com>
2022-08-20 17:28:23 +0800

    jd_speed_wabao.ts

--- jd_cash_wechat.ts
+++ jd_cash_wechat.ts
@@ -22,15 +22,15 @@ class Jd_cash_wechat extends JDHelloWorld {
     await this.run(this)
   }
 
-  async doSign() {
-    let body = await this.cashDoSign()
-    let data: any = await this.post('https://api.m.jd.com/client.action?functionId=cash_sign', body, {
-      'Host': 'api.m.jd.com',
-      'Cookie': this.user.cookie,
-      'user-agent': this.user.UserAgent,
-    })
-    console.log(data.data.bizMsg)
-  }
+  // async doSign() {
+  //   let body = await this.cashDoSign()
+  //   let data: any = await this.post('https://api.m.jd.com/client.action?functionId=cash_sign', body, {
+  //     'Host': 'api.m.jd.com',
+  //     'Cookie': this.user.cookie,
+  //     'user-agent': this.user.UserAgent,
+  //   })
+  //   console.log(data.data.bizMsg)
+  // }
 
   async api(fn: string, body: object) {
     await this.wait(1000)
@@ -53,11 +53,11 @@ class Jd_cash_wechat extends JDHelloWorld {
     let res: any
     try {
       res = await this.api('cash_mob_home', {"isLTRedPacket": "1"})
-      if (res.data.result.signedStatus !== 1) {
-        console.log('开始签到')
-        await this.doSign()
-        console.log('签到成功')
-      }
+      // if (res.data.result.signedStatus !== 1) {
+      //   console.log('开始签到')
+      //   await this.doSign()
+      //   console.log('签到成功')
+      // }
       for (let t of res.data.result.taskInfos) {
         if (t.doTimes !== t.times) {
           console.log(t.name)
@@ -91,49 +91,49 @@ class Jd_cash_wechat extends JDHelloWorld {
     }
   }
 
-  async help(users: User[]) {
-    let shareCodeHW: any = [], shareCode: CODE[] = []
-    this.o2s(this.shareCodeSelf, '内部助力')
-    let res: any
-
-    for (let user of users) {
-      try {
-        this.user = user
-        if (shareCodeHW.length === 0) {
-          shareCodeHW = this.getshareCodeHW('cash')
-        }
-        if (user.index === 0) {
-          shareCode = [...shareCodeHW, ...this.shareCodeSelf]
-        } else {
-          shareCode = [...this.shareCodeSelf, ...shareCodeHW]
-        }
-
-        for (let code of shareCode) {
-          console.log(`账号${user.index + 1} ${user.UserName} 去助力 ${code.inviteCode}`)
-          res = await this.api('redpack_limited_assist', {"inviteCode": code.inviteCode, "shareDate": code.shareDate})
-          console.log(res.data?.result?.limitTimeAssist?.tips)
-          if (res.data?.result?.limitTimeAssist?.assistCode === '207') {
-            break
-          }
-        }
-      } catch (e) {
-        console.log('error', e.message)
-      }
-    }
-
-    for (let user of users) {
-      try {
-        this.user = user
-        console.log(`账号${user.index + 1} ${user.UserName}`)
-        for (let i = 1; i < 5; i++) {
-          res = await this.api('cash_open_limited_redpacket', {"node": i})
-          console.log(res.data)
-        }
-      } catch (e) {
-        console.log('error', e.message)
-      }
-    }
-  }
+  // async help(users: User[]) {
+  //   let shareCodeHW: any = [], shareCode: CODE[] = []
+  //   this.o2s(this.shareCodeSelf, '内部助力')
+  //   let res: any
+  //
+  //   for (let user of users) {
+  //     try {
+  //       this.user = user
+  //       if (shareCodeHW.length === 0) {
+  //         shareCodeHW = this.getshareCodeHW('cash')
+  //       }
+  //       if (user.index === 0) {
+  //         shareCode = [...shareCodeHW, ...this.shareCodeSelf]
+  //       } else {
+  //         shareCode = [...this.shareCodeSelf, ...shareCodeHW]
+  //       }
+  //
+  //       for (let code of shareCode) {
+  //         console.log(`账号${user.index + 1} ${user.UserName} 去助力 ${code.inviteCode}`)
+  //         res = await this.api('redpack_limited_assist', {"inviteCode": code.inviteCode, "shareDate": code.shareDate})
+  //         console.log(res.data?.result?.limitTimeAssist?.tips)
+  //         if (res.data?.result?.limitTimeAssist?.assistCode === '207') {
+  //           break
+  //         }
+  //       }
+  //     } catch (e) {
+  //       console.log('error', e.message)
+  //     }
+  //   }
+  //
+  //   for (let user of users) {
+  //     try {
+  //       this.user = user
+  //       console.log(`账号${user.index + 1} ${user.UserName}`)
+  //       for (let i = 1; i < 5; i++) {
+  //         res = await this.api('cash_open_limited_redpacket', {"node": i})
+  //         console.log(res.data)
+  //       }
+  //     } catch (e) {
+  //       console.log('error', e.message)
+  //     }
+  //   }
+  // }
 }
 
 new Jd_cash_wechat().init().then()
--- jd_speed_wabao.ts
+++ jd_speed_wabao.ts
@@ -29,7 +29,7 @@ class Jd_speed_wabao extends JDHelloWorld {
 
   async api(fn: string, body: object) {
     let timestamp: number = Date.now()
-    let h5st: string = this.h5stTool.__genH5st({
+    let h5st: string = await this.h5stTool.__genH5st({
       appid: 'activities_platform',
       body: JSON.stringify(body),
       client: 'ios',
@@ -47,9 +47,13 @@ class Jd_speed_wabao extends JDHelloWorld {
   }
 
   async main(user: User) {
+    if (!process.env.FP_8DD95) {
+      console.log('未设置环境变量 FP_8DD95')
+      process.exit()
+    }
     this.user = user
     this.user.UserAgent = `jdltapp;iPhone;3.9.2;Mozilla/5.0 (iPhone; CPU iPhone OS ${this.getIosVer()} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1;`
-    this.h5stTool = new H5ST("8dd95", this.user.UserAgent, process.env.FP_8DD95 || '', 'https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw', 'https://bnzf.jd.com', this.user.UserName)
+    this.h5stTool = new H5ST("8dd95", this.user.UserAgent, process.env.FP_8DD95, 'https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw', 'https://bnzf.jd.com', this.user.UserName)
     await this.h5stTool.__genAlgo()
 
     let res: any, data: any
@@ -87,7 +91,6 @@ class Jd_speed_wabao extends JDHelloWorld {
       console.log(`\n开始【京东账号${user.index + 1}】${user.UserName}\n`)
 
       try {
-        await this.h5stTool.__genAlgo()
         if (shareCodesHW.length === 0) {
           shareCodesHW = await this.getshareCodeHW('fcwb')
         }
@@ -96,82 +99,26 @@ class Jd_speed_wabao extends JDHelloWorld {
         } else {
           shareCodes = [...this.shareCodesSelf, ...shareCodesHW]
         }
-        shareCodes = [{"inviter": "IReO3ad-dyrjil-pq4FZeg", "inviteCode": "0ba9527b9ad24555af6e5db7ba9c6bac65791660644915062"}, {"inviter": "JJ4liT2gNT4b_syynalMQuz7mATs6v030333kI9K9fw", "inviteCode": "e6688787bdba4b9fbd457a3e81ce37bf54461660645189599"}, {"inviter": "H3GVctNZoCwosfA0Gv_0BA", "inviteCode": "9186425e24a04e298599bd23b7d912df81181660645195069"}, {"inviter": "BXuxzLhNISFc5jhjMhjaEw", "inviteCode": "a410aa62d8b64d818a8dc85af8be9e5d42161660645200560"}]
-
         for (let code of shareCodes) {
           console.log(`账号${user.index + 1} ${user.UserName} 去助力 ${code.inviteCode}`)
-          this.h5stTool = new H5ST("8dd95", this.user.UserAgent, process.env.FP_8DD95 || '', `https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw&inviterId=${code.inviter}&inviterCode=${code.inviteCode}&utm_source=iosapp&utm_medium=liteshare&utm_campaign=&utm_term=Qqfriends&ad_od=share`, 'https://bnzf.jd.com', user.UserName)
+          this.h5stTool = new H5ST("8dd95", this.user.UserAgent, process.env.FP_8DD95, `https://bnzf.jd.com/?activityId=pTTvJeSTrpthgk9ASBVGsw&inviterId=${code.inviter}&inviterCode=${code.inviteCode}&utm_source=iosapp&utm_medium=liteshare&utm_campaign=&utm_term=Qqfriends&ad_od=share`, 'https://bnzf.jd.com', user.UserName)
           await this.h5stTool.__genAlgo()
           res = await this.api('happyDigHelp', {"linkId": "pTTvJeSTrpthgk9ASBVGsw", "inviter": code.inviter, "inviteCode": code.inviteCode})
           if (res.code === 0) {
             console.log('助力成功')
-            await this.wait(2000)
+            await this.wait(4000)
             break
           } else if (res.code === 16144) {
             console.log('上限')
-            await this.wait(2000)
+            await this.wait(4000)
             break
           } else {
             console.log(res.code, res.errMsg)
-            await this.wait(2000)
+            await this.wait(4000)
           }
         }
       } catch (e) {
         console.log('error', e)
-      }
-    }
-
-    for (let user of users) {
-      this.user.cookie = user.cookie
-      console.log(`\n开始【京东账号${user.index + 1}】${user.UserName}\n`)
-      await this.h5stTool.__genAlgo()
-      res = await this.api('happyDigHome', {"linkId": "pTTvJeSTrpthgk9ASBVGsw"})
-      let blood: number = res.data.blood, gameOver: boolean = false
-      if (blood <= 1) gameOver = true
-      console.log('❤️', blood)
-
-      for (let round = 1; round < 4; round++) {
-        if (gameOver) break
-        for (let i = 0; i < 4; i++) {
-          try {
-            if (gameOver) {
-              console.log('能量剩余1，跳过 A')
-              break
-            }
-            for (let j = 0; j < 4; j++) {
-              if (gameOver) {
-                console.log('能量剩余1，跳过 B')
-                break
-              }
-              res = await this.api('happyDigDo', {"round": round, "rowIdx": i, "colIdx": j, "linkId": "pTTvJeSTrpthgk9ASBVGsw"})
-
-              if (res.data.chunk.type === 1) {
-                console.log('👎')
-              } else if (res.data.chunk.type === 2) {
-                console.log('🧧', parseFloat(res.data.chunk.value))
-              } else if (res.data.chunk.type === 3) {
-                console.log('💰', parseFloat(res.data.chunk.value))
-              } else if (res.data.chunk.type === 4) {
-                console.log('💣')
-              } else {
-                this.o2s(res, '🤔️')
-              }
-              await this.wait(3000)
-
-              res = await this.api('happyDigHome', {"linkId": "pTTvJeSTrpthgk9ASBVGsw"})
-              console.log('❤️', res.data.blood)
-              if (res.data.blood === 1) {
-                gameOver = true
-                console.log('能量剩余1，退出')
-                break
-              }
-              await this.wait(2000)
-            }
-          } catch (e) {
-            console.log('error', e)
-            gameOver = true
-          }
-        }
       }
     }
   }
