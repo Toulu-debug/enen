b671c06bebb9052d30a6d9338544af2da5f751aa
JDHelloWorld <jerfy286@qq.com>
2022-09-25 22:55:29 +0800

    jd_mofang.ts

--- jd_mofang.ts
+++ jd_mofang.ts
@@ -1,9 +1,11 @@
 import {User, JDHelloWorld} from "./TS_JDHelloWorld"
-import {Log} from "./log";
+import {logs} from "./test/jinli_log";
+import {getRandomNumberByRange} from "./TS_USER_AGENTS";
 
 class Mofang extends JDHelloWorld {
   user: User
-  mfTool: Log
+  random: string
+  log: string
 
   constructor() {
     super()
@@ -30,16 +32,14 @@ class Mofang extends JDHelloWorld {
     })
   }
 
-  async getLog(): Promise<string> {
-    return await this.mfTool.main()
+  getLog() {
+    let n: number = getRandomNumberByRange(0, logs.length)
+    this.log = logs[n].match(/"log":"(.*)"/)[1]
+    this.random = logs[n].match(/"random":"(\d+)"/)[1]
   }
 
   async main(user: User) {
-    /*
     this.user = user
-    this.mfTool = new Log('50091', 'doInteractiveAssignment', 'XMFhPageh5')
-    await this.mfTool.init()
-
     let log: string = '', res: any
     res = await this.api('getInteractionHomeInfo', {"sign": "u6vtLQ7ztxgykLEr"})
     let taskConfig_projectId: string = res.result.taskConfig.projectId
@@ -53,8 +53,8 @@ class Mofang extends JDHelloWorld {
           if (t.ext.sign1.status === 1) {
             let signDay: number = t.ext.sign1.signList?.length || 0, type: number = t.rewards[signDay].rewardType
             console.log(signDay, type)
-            log = await this.getLog()
-            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": "1", "actionType": "", "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFhPageh5"}})
+            this.getLog()
+            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": "1", "actionType": "", "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFhPageh5"}})
             console.log('签到成功')
           } else {
             console.log('已签到')
@@ -64,14 +64,12 @@ class Mofang extends JDHelloWorld {
         for (let proInfo of t.ext.shoppingActivity || t.ext.browseShop || []) {
           if (proInfo.status === 1) {
             console.log(t.assignmentName)
-            log = await this.getLog()
-            console.log(log)
-
-            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 1, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFhPageh5"}})
+            this.getLog()
+            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 1, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFhPageh5"}})
             console.log(res.msg)
             await this.wait(t.ext.waitDuration * 1000 || 1000)
-            log = await this.getLog()
-            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 0, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFhPageh5"}})
+            this.getLog()
+            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 0, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFhPageh5"}})
             console.log(res.msg)
           }
         }
@@ -79,8 +77,8 @@ class Mofang extends JDHelloWorld {
         for (let proInfo of t.ext.productsInfo || t.ext.addCart || []) {
           if (proInfo.status === 1) {
             console.log(t.assignmentName)
-            log = await this.getLog()
-            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 0, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFhPageh5"}})
+            this.getLog()
+            res = await this.api('doInteractiveAssignment', {"encryptProjectId": taskConfig_projectId, "encryptAssignmentId": t.encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": proInfo.itemId, "actionType": 0, "completionFlag": "", "ext": {}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFhPageh5"}})
             console.log(res.msg)
             if (res.msg === '任务已完成') {
               break
@@ -97,8 +95,8 @@ class Mofang extends JDHelloWorld {
     console.log('碎片进度', res.exchangeRestScoreMap["368"])
     let exchangeRestScoreMap368: number = res.exchangeRestScoreMap["368"]
     for (let i = 1; i < Math.floor(exchangeRestScoreMap368 / 6); i++) {
-      log = await this.getLog()
-      res = await this.api('doInteractiveAssignment', {"encryptProjectId": giftConfig_projectId, "encryptAssignmentId": "wE62TwscdA52Z4WkpTJq7NaMvfw", "sourceCode": "acexinpin0823", "itemId": "", "actionType": "", "completionFlag": "", "ext": {"exchangeNum": 1}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFhPageh5"}})
+      this.getLog()
+      res = await this.api('doInteractiveAssignment', {"encryptProjectId": giftConfig_projectId, "encryptAssignmentId": "wE62TwscdA52Z4WkpTJq7NaMvfw", "sourceCode": "acexinpin0823", "itemId": "", "actionType": "", "completionFlag": "", "ext": {"exchangeNum": 1}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFhPageh5"}})
       console.log('合成魔方', res.rewardsInfo.successRewards['1'].quantity)
       if (i === 20) break
     }
@@ -111,17 +109,16 @@ class Mofang extends JDHelloWorld {
     exchangeRestScoreMap367 >= 4 ? arr.push('khdCzL9YRdYjh3dWFXfZLteUTYu') : ''
     exchangeRestScoreMap367 >= 24 ? arr.push('JkfeMeE5JGmkXiTeJZGzcAWv5cr') : ''
 
-    for (let encryptAssignmentId of arr) {
-      log = await this.getLog()
-      res = await this.api('doInteractiveAssignment', {"encryptProjectId": giftConfig_projectId, "encryptAssignmentId": encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": "", "actionType": "", "completionFlag": "", "ext": {"exchangeNum": 1}, "extParam": {"businessData": {"random": log.match(/"random":"(\d+)"/)[1]}, "signStr": log.match(/"log":"(.*)"/)[1], "sceneid": "XMFDHh5"}})
-      if (res.subCode === '0') {
-        console.log('兑换成功', res.rewardsInfo.successRewards['3'][0].rewardName)
-      } else {
-        console.log('兑换失败', res.msg)
-        break
-      }
-    }
-  */
+    // for (let encryptAssignmentId of arr) {
+    //    this.getLog()
+    //   res = await this.api('doInteractiveAssignment', {"encryptProjectId": giftConfig_projectId, "encryptAssignmentId": encryptAssignmentId, "sourceCode": "acexinpin0823", "itemId": "", "actionType": "", "completionFlag": "", "ext": {"exchangeNum": 1}, "extParam": {"businessData": {"random": this.random}, "signStr": this.log, "sceneid": "XMFDHh5"}})
+    //   if (res.subCode === '0') {
+    //     console.log('兑换成功', res.rewardsInfo.successRewards['3'][0].rewardName)
+    //   } else {
+    //     console.log('兑换失败', res.msg)
+    //     break
+    //   }
+    // }
   }
 }
 
