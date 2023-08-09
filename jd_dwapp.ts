88494441a6ebd69b4dc65e3b878e735db91a4774
Toulu-debug <77717629+JDHelloWorld@users.noreply.github.com>
2023-08-09 22:44:06 +0800

    jd_dwapp.ts

--- jd_dwapp.ts
+++ jd_dwapp.ts
@@ -15,66 +15,57 @@ class Jd_dwapp extends JDHelloWorld {
     await this.run(this)
   }
 
-  async getTaskList() {
-    return await this.post('https://api.m.jd.com/api?functionId=dwapp_task_dwList', new URLSearchParams({
+  async api(fn: string, body: object) {
+    return await this.post(`https://api.m.jd.com/api?functionId=${fn}`, new URLSearchParams({
       'appid': 'h5-sep',
-      'functionId': 'dwapp_task_dwList',
-      'body': JSON.stringify(this.getEncStr('dwapp_task_dwList', {})),
+      'functionId': fn,
+      'body': JSON.stringify(this.getEncStr(fn, body)),
       'client': 'm',
       'clientVersion': '6.0.0'
     }), {
       'Host': 'api.m.jd.com',
-      'Cookie': this.user.cookie,
       'Origin': 'https://prodev.m.jd.com',
+      'User-Agent': this.user.UserAgent,
       'Referer': 'https://prodev.m.jd.com/mall/active/eEcYM32eezJB7YX4SBihziJCiGV/index.html',
-      'user-agent': this.user.UserAgent
+      'Cookie': this.user.cookie
     })
   }
 
-  async api(fn: string, body: object) {
-    if (['dwList', 'dwRecord', 'dwReceive'].includes(fn))
-      fn = `task/${fn}`
-    body = this.getEncStr(fn, body)
-    await this.wait(2000)
-    return await this.post(`https://dwapp.jd.com/user/${fn}`, JSON.stringify(body), {
+  async task(fn: string, body: object) {
+    let url: string = fn === 'dwSignInfo' ? fn : `task/${fn}`
+    return await this.post(`https://dwapp.jd.com/user/${url}`, this.getEncStr(fn, body), {
       'Host': 'dwapp.jd.com',
-      'Cookie': this.user.cookie,
-      'content-type': 'application/json',
-      'origin': 'https://prodev.m.jd.com',
-      'user-agent': this.user.UserAgent,
-      'referer': 'https://prodev.m.jd.com/'
+      'Origin': 'https://prodev.m.jd.com',
+      'User-Agent': this.user.UserAgent,
+      'Referer': 'https://prodev.m.jd.com/mall/active/eEcYM32eezJB7YX4SBihziJCiGV/index.html',
+      'Cookie': this.user.cookie
     })
   }
 
   async main(user: User) {
     this.user = user
     let res: any
-    res = await this.api('dwSignInfo', {})
-
-    if (res.data.signInfo.signStatus === 0) {
-      res = await this.api('dwSign', {})
+    res = await this.task('dwSignInfo', {})
+    if (res.data.signInfo.signStatus !== 1) {
+      res = await this.api('DATAWALLET_USER_SIGN', {})
       console.log('签到成功', res.data.signInfo.signNum)
     } else {
       console.log('已签到')
     }
 
-    res = await this.getTaskList()
+    res = await this.api('dwapp_task_dwList', {})
     for (let t of res.data) {
       if (t.viewStatus === 0) {
-        res = await this.api('dwRecord', {id: t.id, "taskType": t.taskType, "agentNum": "m", "followChannelStatus": ""})
+        console.log(t.name)
+        res = await this.task('dwRecord', {id: t.id, "taskType": t.taskType, "agentNum": "m", "followChannelStatus": ""})
         console.log(res.msg)
-        res = await this.api('dwReceive', {id: t.id})
+        await this.wait(4000)
+        res = await this.task('dwReceive', {id: t.id})
         console.log(res?.data?.giveScoreNum)
       }
+      await this.wait(6000)
     }
-
-    res = await this.getTaskList()
-    for (let t of res.data) {
-      if (t.viewStatus === 2) {
-        res = await this.api('dwReceive', {id: t.id})
-        console.log(res?.data?.giveScoreNum)
-      }
-    }
+    await this.wait(120000)
   }
 }
 
