727ef2fcc72a8d578fcdf4aa6e2b83425351a283
Toulu-debug <77717629+JDHelloWorld@users.noreply.github.com>
2024-05-31 16:29:13 +0800

    jd_wechat_sign

--- jd_wechat_sign.ts
+++ jd_wechat_sign.ts
@@ -1,73 +1,79 @@
 /**
- * 微信小程序签到红包
- * FP_9A38A
- * cron: 8 10 * * *
+ * 微信小程序签到
  */
 
-import {H5ST} from "./utils/h5st_pro"
-import {User, JDHelloWorld} from "./TS_JDHelloWorld";
+import {User, JDHelloWorld} from "./TS_JDHelloWorld"
+import {JSDOM, ResourceLoader, VirtualConsole} from "jsdom";
+import {readFileSync} from "fs";
+import CryptoJS from "crypto-js";
 
-class Jd_wechat_sign extends JDHelloWorld {
+class Jd_fruit_help extends JDHelloWorld {
   user: User
-  h5stTool: H5ST
-  fp: string
+  appId: string
+  htstTool: any
 
   constructor() {
-    super("微信签到");
+    super();
   }
 
   async init() {
-    try {
-      this.fp = process.env.FP_9A38A ?? await this.getFp()
-    } catch (e) {
-      console.log('FP Error: ', e.message)
-      process.exit(0)
-    }
     await this.run(this)
   }
 
-  async task(fn: string, body: object, signComponent: string) {
-    let h5st: string = await this.h5stTool.__genH5st({
-      appid: "hot_channel",
-      body: JSON.stringify(body),
-      client: "android",
-      clientVersion: "7.22.240",
-      functionId: signComponent,
+  async h5stToolInit() {
+    let dom = new JSDOM(`<body><script>${readFileSync('utils/h5st_47.js').toString()}</script></body>`, {
+      url: "http://localhost",
+      userAgent: this.user.UserAgent,
+      runScripts: "dangerously",
+      resources: new ResourceLoader({
+        userAgent: this.user.UserAgent
+      }),
+      includeNodeLocations: true,
+      storageQuota: 1000000000,
+      pretendToBeVisual: true,
+      virtualConsole: new VirtualConsole()
     })
-    return await this.post(`https://api.m.jd.com/scanTask/${fn}`, `client=android&clientVersion=7.22.240&functionId=${signComponent}&appid=hot_channel&body=${encodeURIComponent(JSON.stringify(body))}&h5st=${h5st}`, {
+    this.htstTool = new dom.window.ParamsSign({appId: this.appId})
+  }
+
+  async api(fn: string, body: object) {
+    let h5st: string = (await this.htstTool.sign({
+      appid: 'hot_channel',
+      body: CryptoJS.SHA256(JSON.stringify(body)).toString(CryptoJS.enc.Hex),
+      client: 'android',
+      clientVersion: '9.17.240',
+      functionId: fn,
+    })).h5st
+    return await this.post('https://api.m.jd.com/signTask/doSignTask', `loginType=2&client=android&clientVersion=9.17.240&functionId=SignComponent_doSignTask&appid=hot_channel&body=${encodeURIComponent(JSON.stringify(body))}&h5st=${h5st}`, {
       'Host': 'api.m.jd.com',
-      'wqreferer': 'http://wq.jd.com/wxapp/pages/market/market2/index',
       'User-Agent': this.user.UserAgent,
-      'Referer': 'https://servicewechat.com/wx91d27dbf599dff74/664/page-frame.html',
-      'Cookie': this.user.cookie
+      'Cookie': this.user.cookie,
+      'xweb_xhr': '1',
+      'X-Rp-Client': 'mini_2.0.0',
+      'X-Referer-Package': 'wx91d27dbf599dff74',
+      'wqreferer': 'http://wq.jd.com/wxapp/pages/marketing/glb/glb/index',
+      'X-Referer-Page': '/pages/marketing/glb/glb/index',
+      'Referer': 'https://servicewechat.com/wx91d27dbf599dff74/752/page-frame.html'
     })
   }
 
   async main(user: User) {
     try {
       this.user = user
-      this.user.UserAgent = `Mozilla/5.0 (iPhone; CPU iPhone OS ${this.getIosVer()} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.28(0x18001c2b) NetType/WIFI Language/zh_CN`
-      let res: any, data: any
-
-      this.h5stTool = new H5ST("9a38a", this.user.UserAgent, this.fp, 'http://wq.jd.com/wxapp/pages/market/market2/index', 'http://wq.jd.com', this.user.UserName);
-      await this.h5stTool.__genAlgo()
-      res = await this.task('doSignTask', {"activityId": "10004", "version": 1}, 'SignComponent_doSignTask')
-      res.success ? console.log('签到奖励', res.data.rewardList[0].discount) : console.log(res.message)
-
-      this.h5stTool = new H5ST("2b5bc", this.user.UserAgent, this.fp, 'http://wq.jd.com/wxapp/pages/market/market2/index', 'http://wq.jd.com', this.user.UserName);
-      await this.h5stTool.__genAlgo()
-      res = await this.task('querySignList', {"activityId": "10004", "version": 1}, 'SignComponent_querySignList')
-      if (!res.data.scanTaskInfo.completionFlag) {
-        data = await this.task('startScanTask', {"itemId": res.data.scanTaskInfo.itemId, "activityId": "10004", "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "actionType": 1, "version": 1}, 'SignComponent_doScanTask')
-        console.log('开始任务', data.message || res.success)
-        await this.wait(8000)
-        data = await this.task('startScanTask', {"activityId": "10004", "actionType": 0, "scanAssignmentId": res.data.scanTaskInfo.scanAssignmentId, "itemId": res.data.scanTaskInfo.itemId, "version": 1}, 'SignComponent_doScanTask')
-        console.log('领取奖励', data.data.rewardList[0].discount)
+      this.user.UserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 MicroMessenger/6.8.0(0x16080000) NetType/WIFI MiniProgramEnv/Mac MacWechat/WMPF MacWechat/3.8.8(0x13080811) XWEB/1216`
+      this.appId = '9a38a'
+      await this.h5stToolInit()
+      let res: any = await this.api('SignComponent_doSignTask', {"activityId": "10004", "version": 1})
+      if (res.success) {
+        console.log('签到天数', res.data.signDays)
+        console.log('获得红包', res.data.rewardList[0].discount)
+      } else {
+        console.log(res.message)
       }
     } catch (e) {
-      console.log(e.message)
+      console.log('Error', e)
     }
   }
 }
 
-new Jd_wechat_sign().init().then()
+new Jd_fruit_help().init().then()
